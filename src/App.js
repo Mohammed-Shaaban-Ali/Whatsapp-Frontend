4050fbcceb6e2800e4c207713d6acb2f3880bbd3
Mohammed Shaaban <ms7500746@gnail.com>
2023-09-10 21:50:07 +0300

    sart project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import Messenger from "./components/Messenger.jsx";
 import "./App.css";
 
 function App() {
-  const clientId =
-    "738914189423-gujq4c8a9gkaktifcthpqpa7rhoa26hp.apps.googleusercontent.com";
-  return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <Messenger />
-    </GoogleOAuthProvider>
-  );
-}
+  //   const clientId =
+  //     "738914189423-gujq4c8a9gkaktifcthpqpa7rhoa26hp.apps.googleusercontent.com";
 
+  return <div>ssss</div>;
+}
 export default App;
