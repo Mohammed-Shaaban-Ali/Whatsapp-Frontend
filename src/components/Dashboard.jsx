733165a3bcb94b880c57e9796a0ee21161108480
Mohammed Shaaban <ms7500746@gnail.com>
2023-09-11 14:32:58 +0300

    handle private route

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,200 +9,178 @@ const Dashboard = () => {
     { name: "test4", status: "available", image: userImage },
   ];
   return (
-    <div className="w-screen flex">
-      <div className="w-[25%] border  h-screen ">
-        <div className="flex justify-center items-center my-8">
-          <div className=" border border-primary p-[2px] rounded-full">
-            <img src={userImage} alt="userImage" width={60} height={60} />
+    <div className="bg-[#e1edff] h-screen flex justify-center items-center">
+      <div className="w-screen flex">
+        <div className="w-[25%] border  h-screen ">
+          <div className="flex justify-center items-center my-8">
+            <div className=" border border-primary p-[2px] rounded-full">
+              <img src={userImage} alt="userImage" width={60} height={60} />
+            </div>
+            <div className="ml-6">
+              <h3 className="text-2xl">Mohammed Shaaban</h3>
+              <p className="text-lg font-light">my account</p>
+            </div>
           </div>
-          <div className="ml-6">
-            <h3 className="text-2xl">Mohammed Shaaban</h3>
-            <p className="text-lg font-light">my account</p>
-          </div>
-        </div>
-        <hr />
-        <div className="mx-12 mt-8">
-          <div className="text-primary text-xl">Messages</div>
-          <div className="">
-            {Contanent?.map((item, index) => (
-              <div
-                key={index}
-                className="flex  items-center py-6 border-b border-b-gray-300"
-              >
-                <div className="cursor-pointer flex items-center">
-                  <div className=" border border-blue-300 p-[2px] rounded-full">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      width={45}
-                      height={45}
-                    />
-                  </div>
-                  <div className="ml-6">
-                    <h3 className="text-2xl font-semibold">{item.name}</h3>
-                    <p className="text-lg font-light text-gray-600">
-                      {item.status}
-                    </p>
+          <hr />
+          <div className="mx-12 mt-8">
+            <div className="text-primary text-xl">Messages</div>
+            <div className="">
+              {Contanent?.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex  items-center py-6 border-b border-b-gray-300"
+                >
+                  <div className="cursor-pointer flex items-center">
+                    <div className=" border border-blue-300 p-[2px] rounded-full">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        width={45}
+                        height={45}
+                      />
+                    </div>
+                    <div className="ml-6">
+                      <h3 className="text-2xl font-semibold">{item.name}</h3>
+                      <p className="text-lg font-light text-gray-600">
+                        {item.status}
+                      </p>
+                    </div>
                   </div>
                 </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[50%] border h-screen bg-white flex flex-col items-center">
+          <div className="w-[75%] bg-secondary h-[60px] rounded-full flex items-center mt-4 mb-4 px-14">
+            <div className=" border border-primary p-[2px] rounded-full">
+              <img src={userImage} alt="userImage" width={40} height={40} />
+            </div>
+            <div className="ml-4">
+              <h3 className="text-base font-semibold">Mohammed Shaaban</h3>
+              <p className="text-sm font-light">my account</p>
+            </div>
+            <div className="cursor-pointer ml-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-phone-plus"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
+                <path d="M15 6h6m-3 -3v6"></path>
+              </svg>
+            </div>
+          </div>
+
+          <div className="h-[75%] w-full border overflow-y-scroll">
+            <div className=" p-14">
+              <div className="max-w-[60%] w-fit bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
               </div>
-            ))}
+              <div className="max-w-[60%] w-fit bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+              <div className="max-w-[60%] w-fit bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores eaque nisi accusamus
+              </div>
+            </div>
+          </div>
+          <div className="p-8 w-full flex items-center">
+            <Input
+              devclasName="w-[75%]"
+              className="rounded-full shadow-md bg-secondary p-2 px-4 border-0 focus:ring-0 focus:bottom-0 outline-none "
+              plasholder="Type a message..."
+            />
+            <div className="ml-4 p-3 cursor-pointer bg-light rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-send"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M10 14l11 -11"></path>
+                <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path>
+              </svg>
+            </div>
+            <div className="ml-3 p-3 cursor-pointer bg-light rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-circle-plus"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                <path d="M9 12h6"></path>
+                <path d="M12 9v6"></path>
+              </svg>
+            </div>
           </div>
         </div>
+
+        <div className="w-[25%] border  h-screen"></div>
       </div>
-
-      <div className="w-[50%] border h-screen bg-white flex flex-col items-center">
-        <div className="w-[75%] bg-secondary h-[60px] rounded-full flex items-center mt-4 mb-4 px-14">
-          <div className=" border border-primary p-[2px] rounded-full">
-            <img src={userImage} alt="userImage" width={40} height={40} />
-          </div>
-          <div className="ml-4">
-            <h3 className="text-base font-semibold">Mohammed Shaaban</h3>
-            <p className="text-sm font-light">my account</p>
-          </div>
-          <div className="cursor-pointer ml-auto">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-phone-plus"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
-              <path d="M15 6h6m-3 -3v6"></path>
-            </svg>
-          </div>
-        </div>
-
-        <div className="h-[75%] w-full border overflow-y-scroll">
-          <div className=" p-14">
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>{" "}
-            <div className="max-w-[55%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-            <div className="max-w-[55%] bg-primary text-white rounded-b-xl rounded-tr-xl p-4 ml-auto mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores eaque nisi accusamus
-            </div>
-          </div>
-        </div>
-        <div className="p-8 w-full flex items-center">
-          <Input
-            devclasName="w-[75%]"
-            className="rounded-full shadow-md bg-secondary p-2 px-4 border-0 focus:ring-0 focus:bottom-0 outline-none "
-            plasholder="Type a message..."
-          />
-          <div className="ml-4 p-3 cursor-pointer bg-light rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-send"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M10 14l11 -11"></path>
-              <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path>
-            </svg>
-          </div>
-          <div className="ml-3 p-3 cursor-pointer bg-light rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-circle-plus"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-              <path d="M9 12h6"></path>
-              <path d="M12 9v6"></path>
-            </svg>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-[25%] border  h-screen"></div>
     </div>
   );
 };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
-const Form = ({ isSignIn = true }) => {
+const Form = ({ isSignIn = false }) => {
   const [data, setData] = useState({
     ...(!isSignIn && {
       name: "",
@@ -11,51 +12,56 @@ const Form = ({ isSignIn = true }) => {
     password: "",
   });
   return (
-    <div className="bg-white w-[550px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
-      <div className="text-4xl font-extrabold ">
-        Weclome {isSignIn && "Back"}
-      </div>
-      <div className="text-xl font-semibold mb-14 ">
-        {isSignIn ? "Sign in to started" : "Sign up now to get started"}
-      </div>
+    <div className="bg-[#e1edff] h-screen flex justify-center items-center">
+      <div className="bg-white w-[550px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+        <div className="text-4xl font-extrabold ">
+          Weclome {isSignIn && "Back"}
+        </div>
+        <div className="text-xl font-semibold mb-14 ">
+          {isSignIn ? "Sign in to started" : "Sign up now to get started"}
+        </div>
 
-      <form className="flex flex-col justify-center items-center w-full">
-        {!isSignIn && (
+        <form className="flex flex-col justify-center items-center w-full">
+          {!isSignIn && (
+            <Input
+              value={data.name}
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+              label="Full Name"
+              name="name"
+              plasholder="Enter Your Full Name"
+              className="mb-6"
+            />
+          )}
           <Input
-            value={data.name}
-            onChange={(e) => setData({ ...data, name: e.target.value })}
-            label="Full Name"
-            name="name"
-            plasholder="Enter Your Full Name"
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+            type="email"
+            label="Email Address"
+            name="email"
+            plasholder="Enter Your Email Address"
             className="mb-6"
           />
-        )}
-        <Input
-          value={data.email}
-          onChange={(e) => setData({ ...data, email: e.target.value })}
-          type="email"
-          label="Email Address"
-          name="email"
-          plasholder="Enter Your Email Address"
-          className="mb-6"
-        />
-        <Input
-          value={data.password}
-          onChange={(e) => setData({ ...data, password: e.target.value })}
-          type="password"
-          label="Password"
-          name="password"
-          plasholder="Enter Your Password"
-          className="mb-10"
-        />
-        <Button className="w-1/2" label={isSignIn ? "Sign in" : "Sign up"} />
-      </form>
-      <div className="mt-2">
-        Alrdy have an account?{" "}
-        <span className="text-primary cursor-pointer underline">
-          {" "}
-          {isSignIn ? "Sign up" : "Sign in"}
-        </span>
+          <Input
+            value={data.password}
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+            type="password"
+            label="Password"
+            name="password"
+            plasholder="Enter Your Password"
+            className="mb-10"
+          />
+          <Button className="w-1/2" label={isSignIn ? "Sign in" : "Sign up"} />
+        </form>
+        <div className="mt-2">
+          Alrdy have an account?{" "}
+          <Link
+            to={isSignIn ? "/user/sign-up" : "/user/sign-in"}
+            className="text-primary cursor-pointer underline"
+          >
+            {" "}
+            {isSignIn ? "Sign up" : "Sign in"}
+          </Link>
+        </div>
       </div>
     </div>
   );
